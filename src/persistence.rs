02c7a542c45f41abdef23b80bb9e4df6e8b95fdc
vismjs <jonas.maas@vector.com>
2022-09-26 22:25:47 +0200

    Added missing file

---
+++ src/persistence.rs
@@ -0,0 +1,17 @@
+use crate::world::World;
+use std::fs::File;
+const FILENAME: &str = "data/world.yml";
+
+pub struct Persistence {}
+
+impl Persistence {
+    pub fn load_world() -> World {
+        let infile = File::open(FILENAME).unwrap();
+        return serde_yaml::from_reader(infile).unwrap();
+    }
+
+    pub fn write_world(world: World) {
+        let outfile = File::create(FILENAME).unwrap();
+        return serde_yaml::from_reader(outfile).unwrap();
+    }
+}
