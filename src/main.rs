97853a2a32a21a91d8220dbe79c4cb22c8734245
vismjs <jonas.maas@vector.com>
2022-09-26 21:45:03 +0200

    Removed obsolete imports

--- src/main.rs
+++ src/main.rs
@@ -8,26 +8,18 @@ mod world;
 mod worlddata;
 
 use log::info;
-use player::Player;
-use processor::Processor;
-use recipe::Recipe;
-use resource::Resource;
 use simple_logger::SimpleLogger;
 use std::fs::File;
 use std::{thread, time::Duration};
 use world::World;
-use worlddata::WorldData;
 
 fn main() {
     SimpleLogger::new().env().init().unwrap();
     info!("=== SIM TEST ===");
 
-    // Create world
+    // Read world
     let infile = File::open("data/world.yml").unwrap();
     let mut world: World = serde_yaml::from_reader(infile).unwrap();
-
-    let outfile = File::create("data/world2.yml").unwrap();
-    serde_yaml::to_writer(outfile, &world).unwrap();
 
     // Sim loop
     let periode = Duration::from_millis(500);
