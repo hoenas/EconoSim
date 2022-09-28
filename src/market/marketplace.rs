f374d9d581acd20faca4bddfc8921417700df8ec
vismjs <jonas.maas@vector.com>
2022-09-28 15:51:42 +0200

    Added get_cheapest_offer function

--- src/market/marketplace.rs
+++ src/market/marketplace.rs
@@ -1,5 +1,5 @@
-use crate::market::offer::Offer;
 use crate::player::Player;
+use crate::{market::offer::Offer, resource::ResourceHandle};
 
 use serde::{Deserialize, Serialize};
 
@@ -13,6 +13,20 @@ pub struct Marketplace {
 impl Marketplace {
     pub fn new() -> Marketplace {
         Marketplace { offers: Vec::new() }
+    }
+
+    pub fn get_cheapest_offer(&self, resource: ResourceHandle) -> Option<&Offer> {
+        let mut cheapest_offer: Option<&Offer> = None;
+        for offer in self.offers.iter() {
+            if offer.resource == resource {
+                if cheapest_offer.is_none() {
+                    cheapest_offer = Some(offer);
+                } else if cheapest_offer.unwrap().price_per_unit > offer.price_per_unit {
+                    cheapest_offer = Some(offer);
+                }
+            }
+        }
+        cheapest_offer
     }
 
     pub fn place_offer(&mut self, offer: Offer) -> OfferHandle {
