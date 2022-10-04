2271f4265ded28df73226343d62914b3b3cb1bd7
hoe nas <hoenas@gmail.com>
2022-10-04 21:39:03 +0200

    Restructured marketplace

--- src/market/marketplace.rs
+++ src/market/marketplace.rs
@@ -1,8 +1,8 @@
 use std::collections::HashMap;
 
+use crate::economy::player::Player;
 use crate::economy::resource::ResourceHandle;
 use crate::market::offer::Offer;
-use crate::player::Player;
 
 use serde::{Deserialize, Serialize};
 
@@ -10,41 +10,38 @@ pub type OfferHandle = usize;
 
 #[derive(Serialize, Deserialize, Default)]
 pub struct Marketplace {
-    pub offers: Vec<Offer>,
-    pub price_index: HashMap<ResourceHandle, Option<f64>>,
+    pub offers: HashMap<OfferHandle, Offer>,
+    pub price_index: HashMap<ResourceHandle, Option<(OfferHandle, f64)>>,
     pub resource_count: ResourceHandle,
+    offer_id: OfferHandle,
 }
 
 impl Marketplace {
     pub fn new() -> Marketplace {
         Marketplace {
-            offers: Vec::new(),
+            offers: HashMap::new(),
             price_index: HashMap::new(),
             resource_count: 0,
+            offer_id: 0,
         }
     }
 
     pub fn update_price_index(&mut self) {
-        for handle in 0..self.resource_count {
-            let offer = self.get_cheapest_offer(handle);
-            if offer.is_none() {
-                self.price_index.insert(handle, None);
-            } else {
-                self.price_index
-                    .insert(handle, Some(offer.unwrap().price_per_unit));
-            }
+        for resource_handle in 0..self.resource_count {
+            let offer = self.get_cheapest_offer(resource_handle);
+            self.price_index.insert(resource_handle, None);
         }
     }
 
-    pub fn get_cheapest_offer(&self, resource: ResourceHandle) -> Option<&Offer> {
-        let mut cheapest_offer: Option<&Offer> = None;
+    pub fn get_cheapest_offer(&self, resource: ResourceHandle) -> Option<(OfferHandle, f64)> {
+        let mut cheapest_offer: Option<(OfferHandle, f64)> = None;
         for offer_handle in 0..self.offers.len() - 1 {
             let offer = self.get_offer_by_handle(offer_handle).unwrap();
             if offer.resource == resource {
                 if cheapest_offer.is_none() {
-                    cheapest_offer = Some(offer);
-                } else if cheapest_offer.unwrap().price_per_unit > offer.price_per_unit {
-                    cheapest_offer = Some(offer);
+                    cheapest_offer = Some((offer_handle, offer.price_per_unit));
+                } else if cheapest_offer.unwrap().1 > offer.price_per_unit {
+                    cheapest_offer = Some((offer_handle, offer.price_per_unit));
                 }
             }
         }
@@ -52,27 +49,28 @@ impl Marketplace {
     }
 
     pub fn place_offer(&mut self, offer: Offer) -> OfferHandle {
-        self.offers.push(offer);
+        self.offer_id += 1;
+        self.offers.insert(self.offer_id, offer);
         self.update_price_index();
-        self.offers.len() - 1
+        self.offer_id
     }
 
     pub fn get_offer_by_handle(&self, offer_handle: OfferHandle) -> Option<&Offer> {
-        if offer_handle < self.offers.len() {
-            Some(&self.offers[offer_handle])
-        } else {
-            None
-        }
+        Some(&self.offers[&offer_handle])
     }
 
-    pub fn accept_offer(&mut self, offer: &Offer, player: &mut Player, amount: f64) {
+    pub fn accept_offer(&mut self, offer: &mut Offer, player: &mut Player, amount: f64) {
         if amount <= offer.amount {
             let price = offer.price_per_unit * amount;
             if price <= player.currency {
+                offer.amount -= amount;
                 player.currency -= price;
-                player.stock.add_to_stock(offer.resource, amount);
+                // Clean up offers and update price index
+                if offer.amount <= 0.0 {
+                    self.offers.retain(|_, v| v.amount <= 0.0);
+                    self.update_price_index();
+                }
             }
-            self.update_price_index();
         }
     }
 }
