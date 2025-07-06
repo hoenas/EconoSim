78689271f59aded1bd034dcdde5cb75687725284
hoe nas <hoenas@gmail.com>
2025-07-06 20:47:34 +0200

    Changed function order

--- src/economy/stock.rs
+++ src/economy/stock.rs
@@ -49,20 +49,6 @@ impl Stock {
         in_stock
     }
 
-    pub fn remove_resources_from_stock_if_possible(
-        &mut self,
-        resource_transactions: &Vec<(ResourceHandle, f64)>,
-    ) -> bool {
-        if self.check_resources_in_stock(resource_transactions) {
-            for (resource, amount) in resource_transactions.iter() {
-                self.remove_resource_from_stock_if_possible(*resource, *amount);
-            }
-            true
-        } else {
-            false
-        }
-    }
-
     pub fn remove_resource_from_stock_if_possible(
         &mut self,
         resource: ResourceHandle,
@@ -71,6 +57,20 @@ impl Stock {
         let value_after_transaction = self.calculate_new_stock_value(resource, amount);
         if value_after_transaction >= 0.0 {
             self.resources.insert(resource, value_after_transaction);
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn remove_resources_from_stock_if_possible(
+        &mut self,
+        resource_transactions: &Vec<(ResourceHandle, f64)>,
+    ) -> bool {
+        if self.check_resources_in_stock(resource_transactions) {
+            for (resource, amount) in resource_transactions.iter() {
+                self.remove_resource_from_stock_if_possible(*resource, *amount);
+            }
             true
         } else {
             false
