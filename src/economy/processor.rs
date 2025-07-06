4428509be34f0d9acd1e1c435638f82a5905b099
hoe nas <hoenas@gmail.com>
2025-07-06 22:06:19 +0200

    Added test and fixed bugs

--- src/economy/processor.rs
+++ src/economy/processor.rs
@@ -18,11 +18,16 @@ impl<'a> Processor {
         let recipe = recipe_data.get_recipe_by_handle(self.recipe).unwrap();
         // Check if transaction can be done
         self.produced_last_tick = false;
-        let transaction = &recipe.ingredients.iter().map(|x| (*x.0, *x.1)).collect();
+        let transaction = &recipe
+            .ingredients
+            .iter()
+            .map(|x| (*x.0, *x.1 * self.production_speed * recipe.production_speed))
+            .collect();
         if self.productive && stock.remove_resources_from_stock_if_possible(transaction) {
             // Transaction can be done, add generated resources to stock
             for (resource, production_factor) in recipe.products.iter() {
-                let amount = production_factor * self.production_speed;
+                let amount: f64 =
+                    production_factor * self.production_speed * recipe.production_speed;
                 stock.add_resource_to_stock(*resource, amount);
             }
             self.produced_last_tick = true;
@@ -67,26 +72,46 @@ mod tests {
             products: products,
         };
         let mut recipe_data = RecipeData::new();
+        let mut recipe2 = recipe.clone();
+        recipe2.production_speed = 2.0;
         recipe_data.add_recipe(recipe);
+        recipe_data.add_recipe(recipe2);
         // Create stock
         let mut stock = Stock::new();
-        stock.add_resource_to_stock(0, 1.0);
-        stock.add_resource_to_stock(1, 2.0);
+        stock.add_resource_to_stock(0, 5.0);
+        stock.add_resource_to_stock(1, 10.0);
 
         // Test
         // First tick - everything should work
         // Resources should be down to zero - except the produced one
         processor.tick(&mut stock, &recipe_data);
         assert!(processor.produced_last_tick);
-        assert_eq!(*stock.resources.get(&0).unwrap(), 0.0);
-        assert_eq!(*stock.resources.get(&1).unwrap(), 0.0);
+        assert_eq!(*stock.resources.get(&0).unwrap(), 4.0);
+        assert_eq!(*stock.resources.get(&1).unwrap(), 8.0);
         assert_eq!(*stock.resources.get(&2).unwrap(), 5.0);
-        // Second tick - producing should not have worked
+        // Second tick - double production
+        processor.production_speed = 2.0;
+        processor.tick(&mut stock, &recipe_data);
+        assert!(processor.produced_last_tick);
+        assert_eq!(*stock.resources.get(&0).unwrap(), 2.0);
+        assert_eq!(*stock.resources.get(&1).unwrap(), 4.0);
+        assert_eq!(*stock.resources.get(&2).unwrap(), 15.0);
+        // Third tick - producing should not have worked
         // Resources should be unchanged
+        processor.production_speed = 3.0;
         processor.tick(&mut stock, &recipe_data);
         assert!(!processor.produced_last_tick);
+        assert_eq!(*stock.resources.get(&0).unwrap(), 2.0);
+        assert_eq!(*stock.resources.get(&1).unwrap(), 4.0);
+        assert_eq!(*stock.resources.get(&2).unwrap(), 15.0);
+        // Fourth tick - producing should work again
+        // Increasing the production speed of the recipe should lead to 0 input resources
+        processor.recipe = 1;
+        processor.production_speed = 1.0;
+        processor.tick(&mut stock, &recipe_data);
+        assert!(processor.produced_last_tick);
         assert_eq!(*stock.resources.get(&0).unwrap(), 0.0);
         assert_eq!(*stock.resources.get(&1).unwrap(), 0.0);
-        assert_eq!(*stock.resources.get(&2).unwrap(), 5.0);
+        assert_eq!(*stock.resources.get(&2).unwrap(), 25.0);
     }
 }
