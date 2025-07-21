14c6b0be15515a55f0179ae166e498f4695aa775
hoe nas <hoenas@gmail.com>
2025-07-21 20:58:00 +0200

    Removed obsolete dependencies

--- src/economy/recipe.rs
+++ src/economy/recipe.rs
@@ -2,9 +2,6 @@ use crate::economy::resource::ResourceHandle;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
-use mockall::automock;
-use mockall_double::double;
-
 pub type RecipeHandle = usize;
 
 #[derive(Serialize, Deserialize, Clone)]
@@ -15,7 +12,6 @@ pub struct Recipe {
     pub production_speed: f64,
 }
 
-#[automock]
 impl Recipe {
     pub fn new(name: String, production_speed: f64) -> Self {
         Self {
