76f8a7b5917275eb326e3469bcbb5bcec8abca15
hoe nas <hoenas@gmail.com>
2025-07-21 22:31:29 +0200

    Added neural network base class

---
+++ src/reinforcement_learning/feed_forward.rs
@@ -0,0 +1,34 @@
+use burn::{
+    backend::Wgpu as B,
+    nn::{Linear, LinearConfig, Relu},
+    prelude::*,
+    record::PrettyJsonFileRecorder,
+};
+use serde::{Deserialize, Serialize};
+
+#[derive(Module, Debug)]
+pub struct FeedForward<B: Backend> {
+    linear1: Linear<B>,
+    linear2: Linear<B>,
+    activation: Relu,
+}
+
+#[derive(Config, Debug)]
+pub struct FeedForwardConfig {
+    inputs: usize,
+    outputs: usize,
+    hidden_size: usize,
+    #[config(default = "0.5")]
+    dropout: f64,
+}
+
+impl FeedForwardConfig {
+    /// Returns the initialized model.
+    pub fn init<B: Backend>(&self, device: &B::Device) -> FeedForward<B> {
+        FeedForward {
+            activation: Relu::new(),
+            linear1: LinearConfig::new(self.inputs, self.hidden_size).init(device),
+            linear2: LinearConfig::new(self.hidden_size, self.outputs).init(device),
+        }
+    }
+}
